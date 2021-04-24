28c182c715bd1ad68e53dca5d2bb9e5748221ec5
yong.huang <921757697@qq.com>
2021-04-24 23:28:36 +0800

    UP--上传startup-sh

---
+++ doc/java-startup/java-package.md
@@ -0,0 +1,9 @@
+# 设置一些常用的sh脚本和工具
+
+1.  配置assembly工具与文件, 在pom.xml中添加文件pom.xml中的内容
+
+添加在项目中
+src\assembly\package.xml
+
+2.  配置shell脚本
+
---
+++ doc/java-startup/sbin/jenkins-start.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+moduleName="anyest-center-admin"
+publishDir=$1
+#PRG_NAME=CenterAdminApplication
+backupDir=$2
+PRG_NAME=CenterAdminApplication
+
+PROFILE=$3
+
+echo "************************"
+echo "*** publishDir=$publishDir ***"
+echo "*** moduleName=$moduleName ***"
+echo "*** backupDir=$backupDir ***"
+echo "*** PRG_NAME=$PRG_NAME ***"
+echo "*** $publishDir$moduleName ***"
+echo "*** pwd=`pwd`  ***"
+echo "************************"
+
+
+
+if [ ! -d  "${publishDir}${moduleName}" ]; then
+    mkdir "${publishDir}${moduleName}"
+fi
+if [ ! -d  "$backupDir" ]; then
+    mkdir "$backupDir"
+fi
+
+cd "${publishDir}${moduleName}"
+echo "pwd=`pwd`"
+source /etc/profile
+
+
+shutdown(){
+    kill -9 ${TPID}
+}
+
+TPID=0
+
+getPID(){
+    TPID=`pgrep -f $PRG_NAME`
+}
+
+getPID
+
+if [[ $TPID -ne 0 ]] ; then
+    shutdown
+fi
+
+i=1
+while [[ $i -le 60 ]]
+  do
+    if [[ $TPID ]];
+        then
+          echo "$PRG_NAME is running as process $process_id"
+          sleep 1
+          i=$(( $i + 1 ))
+        else
+          echo "$PRG_NAME is end"
+          break
+    fi
+  done
+
+echo "***********************************************"
+echo "***   server stoped,   begin startting      ***"
+echo "***********************************************"
+
+cd ..
+#文件夹数据备份
+curDate=`date +%Y-%m-%d%H:%M:%S`
+backFileName="${moduleName}-${curDate}"
+#进入备份文件夹
+cd ${backupDir}
+#判断文件是否存在 -f文件 -d文件夹
+if [[ -d $backFileName ]]; then
+echo "*********** $backFileName 存在并移除 *********** "
+rm -rf $backFileName
+fi
+
+cd ${publishDir}
+
+#旧tar包文件夹重命名移动到bak备份文件夹
+mv ${moduleName} "${backupDir}/${backFileName}"
+mkdir ${moduleName}
+
+sleep 1
+`tar -zxvf ${moduleName}*.tar.gz --strip-components 1 -C "${publishDir}${moduleName}"`
+sleep 20
+
+echo "cd ${publishDir}${moduleName}"
+
+cd ${moduleName}
+chmod +x sbin/startup.sh
+sh sbin/startup.sh $3
+
